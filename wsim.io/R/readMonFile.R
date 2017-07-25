a2bb32ad271152f229036322b5172a9cc9ba7956
Dan Baston <dbaston@isciences.com>
2017-07-25 13:58:40 -0400

    Import function to read .mon file, update tests

---
+++ wsim.io/R/readMonFile.R
@@ -0,0 +1,40 @@
+#' Read a gridded binary .mon file
+#'
+#' Read a gridded binary file of the format described at
+#' ftp://ftp.cpc.ncep.noaa.gov/wd51yf/global_monthly/gridded_binary/README.txt
+#'
+#' The file must be global in extent, with 0.5-degree resolution.
+#'
+#' @param filename filename to read
+#' @param na.value NODATA value, to be replaced with NA
+#' @param RasterLayer representing the contents of the .mon file
+readMonFile <- function(filename, na.value=-999) {
+    fh <- file(filename, 'rb')
+
+    # Maximum number of values should be 259200, but some files have more.
+    data <- readBin(fh, n = 260000, what = 'numeric', size = 4, endian = 'big')
+    close(fh)
+
+    # Check length of data
+    if ( length(data) == 259202 ){
+        data <- data[-c(1, 259202)]
+        warning(basename(filename), ' had 2 extra values.  Removed first and last\n')
+    } else if (length(data) > 259202) {
+        stop(basename(filename), ' had ', length(data), ' values.  I don\'t know what to do with it.\n')
+    }
+
+    # Swap out NODATA value
+    data[data == na.value] <- NA
+
+    nx <- 360 / 0.5  # Specify n longitude grid cells
+    ny <- 180 / 0.5  # Specify n latitude grid cells
+
+    # Flip and rotate the data
+    rast <- raster::raster(nrows = ny, ncols = nx, xmn = 0, xmx = 360)
+    rast <- raster::setValues(rast, data)
+
+    rast <- raster::flip(rast, direction = 'y')
+    rast <- raster::rotate(rast)
+
+    return(rast)
+}
--- wsim.io/tests/testthat/test_io_regression.R
+++ wsim.io/tests/testthat/test_io_regression.R
@@ -7,8 +7,24 @@ test_that("We can read a file of NCEP daily precipitation", {
 
   precip <- readNCEP_Daily_P(filename)
 
-  expect_equal(dim(precip), c(720, 360)) # 0.5-degree lat/lon
-  expect_equal(precip[269, 214]/10, 6.842, tolerance=1e-3) # 6.8 mm of rain in Burlington, VT
+  row_btv <- (180+73.2)*720/360 # 0.5-degree cells, starting at antimeridian and working west
+  col_btv <- (90-44.5)*360/180  # 0.5-degree cells, starting at north pole
+
+  expect_equal(dim(precip), c(720, 360))       # 0.5-degree lon/lat
+  expect_equal(precip[row_btv, col_btv]/10, 0) # no rain in Burlingotn, VT
 
   expect_equal(max(precip, na.rm = TRUE), 1076.367, tolerance=1e-3)
+})
+
+test_that("We can read a gridded binary .mon file", {
+  filename <- '/mnt/fig/WSIM/WSIM_source_V1.2/NCEP/originals/t.201701.mon'
+
+  temp <- readMonFile(filename)
+
+  row_btv <- (90-44.5)*360/180 # 0.5-degree cells, starting at north pole
+  col_btv <- (180-73.2)*720/360 # 0.5-degree cells, starting at antimeridian and working east
+
+  expect_equal(unname(temp[row_btv, col_btv]), -3.21, tolerance=1e-3) # 26.2 F in Burlington, VT
+  expect_equal(raster::extent(temp), raster::extent(-180, 180, -90, 90))
+  expect_equal(raster::projection(temp), raster::projection(raster::raster())) # wgs84
 })
