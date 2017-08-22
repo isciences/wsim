7be5712a6df9d93aaf9d169d13a040a64cea901b
Dan Baston <dbaston@isciences.com>
2017-08-22 10:20:57 -0400

    Add utility function to check if a file is writable

---
+++ wsim.io/R/can_write.R
@@ -0,0 +1,30 @@
+#' Determine if a file can be opened for writing
+#'
+#' If the file exists, check if we have write
+#' permissions on it.
+#'
+#' If the file does not exist, try creating a file
+#' of that name, and then remove it.
+#'
+#' @param filename filename to test for write access
+#' @return TRUE if the file can be opened for writing,
+#'         FALSE otherwise
+#' @export
+can_write <- function(filename) {
+  if (file.exists(filename)) {
+    # File exists, can we write to it?
+    return(unname(file.access(filename, mode=2)) == 0)
+  } else {
+    tryCatch({
+      file.create(filename)
+      while(!file.exists(filename)) {
+        Sys.sleep(0.005)
+      }
+      file.remove(filename)
+      return(TRUE);
+    }, error=function() {
+      print('problem')
+      return(FALSE);
+    })
+  }
+}
