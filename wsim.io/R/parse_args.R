9062ccf4452609a78c645638f8da9d6a64438444
Daniel Baston <dbaston@gmail.com>
2017-09-11 12:20:37 -0400

    Allow overriding args in parser

--- wsim.io/R/parse_args.R
+++ wsim.io/R/parse_args.R
@@ -5,11 +5,12 @@
 #' status=1.
 #'
 #' @param usage a docopt string describing program usage
+#' @param args a list of command-line arguments
 #' @return a list of parsed arguments
 #'
 #' @export
-parse_args <- function(usage) {
-  tryCatch(docopt::docopt(usage), error=function(e) {
+parse_args <- function(usage, args=commandArgs(TRUE)) {
+  tryCatch(docopt::docopt(usage, args), error=function(e) {
     write('Error parsing args.', stderr())
     write(usage, stdout())
     quit(status=1)
