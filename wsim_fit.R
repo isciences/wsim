cd338972d97a98646250401fe39b2adaf3eed81b
Daniel Baston <dbaston@isciences.com>
2017-09-13 15:24:16 -0400

    Update wsim_fit to make use of new IO functions

--- wsim_fit.R
+++ wsim_fit.R
@@ -1,11 +1,5 @@
 #!/usr/bin/env Rscript
 
-suppressMessages({
-  require(wsim.distributions)
-  require(wsim.io)
-  require(abind)
-})
-
 '
 Fit statistical distributions.
 
@@ -17,11 +11,11 @@ Usage: wsim_fit (--distribution=<dist>) (--input=<file>)... (--output=<file>) [-
 --cores <num>         Number of CPU cores to use [default: 1]
 '->usage
 
-main <- function(args) {
-  args <- parse_args(usage, args)
+main <- function(raw_args) {
+  args <- wsim.io::parse_args(usage, raw_args)
 
   outfile <- args$output
-  if (!can_write(outfile)) {
+  if (!wsim.io::can_write(outfile)) {
     die_with_message("Cannot open ", outfile, " for writing.")
   }
 
@@ -30,20 +24,27 @@ main <- function(args) {
     parallel::setDefaultCluster(c1)
   }
 
-  inputs <- lapply(expand_inputs(args$input), wsim.io::load_matrix)
-  extent <- attr(inputs[[1]], 'extent')
+  inputs_stacked <- wsim.io::read_vars_to_cube(wsim.io::expand_inputs(args$input))
+  extent <- attr(inputs_stacked, 'extent')
 
-  inputs_stacked <- abind(inputs, along=3)
+  cat('Read', dim(inputs_stacked)[[3]], 'inputs.\n')
 
   distribution <- tolower(args$distribution)
 
   if (distribution == 'gev') {
-    fits <- fitGEV(inputs_stacked)
+    fits <- wsim.distributions::fitGEV(inputs_stacked)
   } else {
-    die_with_message(distribution, " is not a supported statistical distribution.")
+    wsim.io::die_with_message(distribution, " is not a supported statistical distribution.")
   }
 
-  write_vars_to_cdf(fits, outfile, extent=extent, attrs=list(list(var=NULL,key="distribution",val=distribution)))
+  wsim.io::write_vars_to_cdf(fits,
+                             outfile,
+                             extent=extent,
+                             attrs=list(
+                               list(var=NULL,key="distribution",val=distribution)
+                             ))
+
+  cat('Wrote fits to ', outfile, '.\n', sep="")
 }
 
 main(commandArgs(TRUE))
