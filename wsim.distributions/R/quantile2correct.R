6a2d64c372831eff5f4a11cb6855e20ead4209c8
Dan Baston <dbaston@isciences.com>
2017-09-14 10:57:19 -0400

    Fix quantile2correct

--- wsim.distributions/R/quantile2correct.R
+++ wsim.distributions/R/quantile2correct.R
@@ -8,7 +8,7 @@
 #' @param obs_fit 3D array with parameters for the observed value distribution
 #' @return matrix with a corrected forecast
 quantile2correct <- function(quant, obs_fit) {
-  apply_dist_to_matrix(obs_fit, quant, function(value, dist_params) {
+  apply_dist_to_array(obs_fit, quant, function(value, dist_params) {
     if (is.na(value)) {
       return(NA)
     }
