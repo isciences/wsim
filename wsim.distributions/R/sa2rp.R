e0956fabab239e37ff868b409c4bd592cdb852b1
Dan Baston <dbaston@isciences.com>
2017-09-20 09:23:06 -0400

    Preserve matrix dimensions in sa2rp

--- wsim.distributions/R/sa2rp.R
+++ wsim.distributions/R/sa2rp.R
@@ -1,6 +1,6 @@
 #' Convert standardized anomaly into a return period
 #'
-#' @param sa vector or RasterLayer of standardized anomalies
+#' @param sa vector or matrix of standardized anomalies
 #' @param min.rp minimum value for clamping return period
 #' @param max.rp maximum value for clamping return period
 #' @return return period
@@ -8,14 +8,8 @@
 sa2rp <- function(sa, min.rp=-1000, max.rp=1000) {
     rp <- sign(sa) / (1 - pnorm(abs(sa)))
 
-    rp <- pmax(min.rp, rp)
-    rp <- pmin(max.rp, rp)
+    rp <- pmax(rp, min.rp)
+    rp <- pmin(rp, max.rp)
 
     return (rp)
 }
-
-methods::setGeneric('sa2rp')
-
-methods::setMethod('sa2rp', methods::signature('RasterLayer'), function(sa) {
-  raster::setValues(sa, sa2rp(raster::getValues(sa)))
-})
