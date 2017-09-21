265808edb9336ef34ea738d0be5afc45af56aaac
Dan Baston <dbaston@isciences.com>
2017-09-21 14:18:34 -0400

    Simplify looping logic

--- wsim_lsm.R
+++ wsim_lsm.R
@@ -18,7 +18,7 @@ Options:
 --wc <file>          file containing soil water holding capacity
 --elevation <file>   file containing elevations
 
---loop <n>         perform n model iterations using the same forcing data
+--loop <n>         perform n model iterations using the same forcing data [default: 1]
 
 Output:
 --results <file>     filename for model results
@@ -38,9 +38,7 @@ main <- function(raw_args) {
   state <- wsim.lsm::read_state_from_cdf(args$state)
   forcings <- sort(wsim.io::expand_inputs(args$forcing))
 
-  if (!is.null(args$loop)) {
-    forcings <- rep.int(forcings, args$loop)
-  }
+  loops <- args$loop
 
   static$area_m2 <- raster::as.matrix(wsim.lsm::cell_areas_m2(raster::raster(static$elevation,
                                                                              xmn=state$extent[1],
@@ -54,36 +52,39 @@ main <- function(raw_args) {
 
   results <- NULL
   iter_num <- 0
-  for (f in forcings) {
-    iter_num <- iter_num + 1
+  for (loop_num in 1:loops) {
+    for (i in 1:length(forcings)) {
+      iter_num <- iter_num + 1
 
-    forcing <- wsim.lsm::read_forcing_from_cdf(f)
-    cat("Running LSM for ", state$yearmon, ", using ", f, " ...", sep="")
-    iter <- wsim.lsm::run(static, state, forcing)
-    cat("done.\n")
+      forcing <- wsim.lsm::read_forcing_from_cdf(forcings[i])
 
-    if (write_all_results) {
-      fname <- gsub("%T", state$yearmon, args$results)
-      fname <- gsub("%N", iter_num, fname)
-      cat("  Writing model results to", fname, "\n")
-      wsim.lsm::write_lsm_values_to_cdf(iter$obs,
-                                        fname,
-                                        wsim.lsm::cdf_attrs)
+      cat("Running LSM for ", state$yearmon, ", using ", forcings[i], " ...", sep="")
+      iter <- wsim.lsm::run(static, state, forcing)
+      cat("done.\n")
+
+      if (write_all_results) {
+        fname <- gsub("%T", state$yearmon, args$results)
+        fname <- gsub("%N", iter_num, fname)
+        cat("  Writing model results to", fname, "\n")
+        wsim.lsm::write_lsm_values_to_cdf(iter$obs,
+                                          fname,
+                                          wsim.lsm::cdf_attrs)
+      }
+
+      if (write_all_states) {
+        fname <- gsub("%T", iter$next_state$yearmon, args$next_state)
+        fname <- gsub("%N", iter_num, fname)
+        cat("  Writing next state to", fname, "\n")
+        wsim.lsm::write_lsm_values_to_cdf(iter$next_state,
+                                          fname,
+                                          wsim.lsm::cdf_attrs)
+      }
+
+      state <- iter$next_state
+      results <- iter$obs
+
+      gc()
     }
-
-    if (write_all_states) {
-      fname <- gsub("%T", iter$next_state$yearmon, args$next_state)
-      fname <- gsub("%N", iter_num, fname)
-      cat("  Writing next state to", fname, "\n")
-      wsim.lsm::write_lsm_values_to_cdf(iter$next_state,
-                                        fname,
-                                        wsim.lsm::cdf_attrs)
-    }
-
-    state <- iter$next_state
-    results <- iter$obs
-
-    gc()
   }
 
   if (!write_all_states) {
