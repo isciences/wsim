71b44a0171cd3961219e049816e7cc279f24dac5
Dan Baston <dbaston@isciences.com>
2017-09-25 15:16:07 -0400

    Fix error in PET calculation

--- wsim.lsm/R/e_potential.R
+++ wsim.lsm/R/e_potential.R
@@ -6,9 +6,5 @@
 #' @return Potential evapotranspiration (mm)
 #' @export
 e_potential <- function(lambda, T, nDays) {
-  # TODO FIXME
-  # The "237.2" number is a typo, copied from the Kepler workspace
-  # so that we can verify consistent output.
-  # The correct number is 273.2
-  nDays * 715.5 * lambda * saturated_vapor_pressure(T) / (T + 237.2)
+  nDays * 715.5 * lambda * saturated_vapor_pressure(T) / (T + 273.15)
 }
