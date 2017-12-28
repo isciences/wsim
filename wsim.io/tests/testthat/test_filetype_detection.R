058ce63d0ab617965299ca97d728cbe36845becd
Dan Baston <dbaston@isciences.com>
2017-12-28 13:55:26 -0500

    Fix test file

--- wsim.io/tests/testthat/test_filetype_detection.R
+++ wsim.io/tests/testthat/test_filetype_detection.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require(testthat)
 
 context('File type detection')
@@ -10,17 +9,3 @@ test_that('Various types of daily precipitation files are recognized', {
   expect_true(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/source/NCEP/Daily_precip/2008/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.20080116.RT.gz'))
   expect_false(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/daily_precip/1989/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.19890402.img'))
 })
-||||||| merged common ancestors
-=======
-require(testthat)
-
-context('File type detection')
-
-test_that('Various types of daily precipitation files are recognized', {
-  expect_true(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/daily_precip/1989/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.19890402.gz'))
-  expect_true(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/daily_precip/2006/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.20060123RT.gz'))
-  expect_true(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/daily_precip/2016/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.20160123.RT'))
-  expect_true(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/source/NCEP/Daily_precip/2008/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.20080116.RT.gz'))
-  expect_false(is_ncep_daily_precip('/mnt/fig_rw/WSIM_DEV/daily_precip/1989/PRCP_CU_GAUGE_V1.0GLB_0.50deg.lnx.19890402.img'))
-})
->>>>>>> master
