41c39782820b9a047011ade28978c175472357d7
Dan Baston <dbaston@isciences.com>
2018-02-27 10:41:11 -0500

    Fix typos in STN30 prep steps

--- workflow/data_sources/stn30.py
+++ workflow/data_sources/stn30.py
@@ -15,12 +15,13 @@ import os
 
 from step import Step
 
-def global_flow_direction(source_dir, filename, resolution):
+def global_flow_direction(source_dir, resolution):
     if resolution != 0.5:
         raise ValueError('Only half-degree resolution is provided by STN-30')
 
     dirname = os.path.join(source_dir, 'STN_30')
-    url = 'global_30_minute_potential_network_v601_asc.zip'
+    filename = os.path.join(dirname, 'g_network.asc')
+    url = 'http://www.wsag.unh.edu/Stn-30/v_6.01/global_30_minute_potential_network_v601_asc.zip'
     zip_path = os.path.join(dirname, url.split('/')[-1])
 
     return [
@@ -38,7 +39,7 @@ def global_flow_direction(source_dir, filename, resolution):
             targets=filename,
             dependencies=zip_path,
             commands=[
-                [ 'unzip', '-j', zip_path, 'global_potential_network_v601_asc/g_network.asc', '-d', dirname ],
+                [ 'unzip', '-j', zip_path, 'global_30_minute_potential_network_v601_asc/g_network.asc', '-d', dirname ],
                 [ 'touch', filename ] # Make extracted date modified > archive date modified
             ]
         )
