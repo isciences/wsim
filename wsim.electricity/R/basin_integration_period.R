e55da207f1fcc4c0c92fe6ab94a25d27d215b214
Dan Baston <dbaston@isciences.com>
2018-08-20 12:13:45 -0400

    Add wsim.electricity R package

---
+++ wsim.electricity/R/basin_integration_period.R
@@ -0,0 +1,25 @@
+# Copyright (c) 2018 ISciences, LLC.
+# All rights reserved.
+#
+# WSIM is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Select an integration period for a basin
+#' 
+#' @param upstream_capacity     upstream storage capacity available for electric
+#'                              power generation
+#' @param expected_monthly_flow typical monthly total blue water, e.g., the location
+#'                              parameter of a fitted distribution
+#' @param available_periods     vector of available integration periods in months              
+basin_integration_period <- function(upstream_capacity, expected_monthly_flow, available_periods) {
+  available_periods[findInterval(upstream_capacity / expected_monthly_flow,
+                                 c(available_periods, Inf),
+                                 all.inside=TRUE)]
+}
---
+++ wsim.electricity/tests/testthat/test_electricity.R
@@ -0,0 +1,37 @@
+# Copyright (c) 2018 ISciences, LLC.
+# All rights reserved.
+#
+# WSIM is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+context('WSIM electricity assessment')
+
+bins <- c(1, 3, 6, 12, 24, 36) 
+
+test_that('when there is no upstream storage, we use a 1-month integration period', {
+  expect_equal(basin_integration_period(0, 100, bins), 1)  
+})
+
+test_that('when there is less than 1 month of upstream storage, we use a 1-month integration period', {
+  expect_equal(basin_integration_period(99, 100, bins), 1)
+})
+
+test_that('when there is less than 3 months of upstream storage, we use a 1-month integration period', {
+  expect_equal(basin_integration_period(299, 100, bins), 1)
+})
+
+test_that('when there are [3, 6) months of upstream storage, we use a 3-month integration period', {
+  expect_equal(basin_integration_period(300, 100, bins), 3)
+  expect_equal(basin_integration_period(599, 100, bins), 3)
+})
+
+test_that('when there are more than 36 months of upstream storage, we use a 36-month integration period', {
+  expect_equal(basin_integration_period(100000, 100, bins), 36)
+})
