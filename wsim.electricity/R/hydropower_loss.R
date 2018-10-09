939bdd6925fd775fb84312da071b232fcec90b58
Dan Baston <dbaston@isciences.com>
2018-10-09 14:28:35 -0400

    Add hydropower loss

---
+++ wsim.electricity/R/hydropower_loss.R
@@ -0,0 +1,23 @@
+# Copyright (c) 2018 ISciences, LLC.
+# All rights reserved.
+#
+# WSIM is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. You may
+# obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Estimate hydropowerloss based on blue water
+#'
+#' @param blue_water observed blue water
+#' @param expected   blue water (e.g., location parameter of fitted distribution)
+#' @export
+hydropower_loss <- function(blue_water, blue_water_expected, exponent) {
+  pct <- (blue_water / blue_water_expected) ^ exponent
+  
+  1 - pmax(pmin(pct, 1.0), 0.0)
+}
