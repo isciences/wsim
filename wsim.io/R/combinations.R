455ef318793f287cf1ed7dc6fedf328fa67d1f45
Dan Baston <dbaston@isciences.com>
2019-02-06 11:40:54 -0500

    Add missing file

---
+++ wsim.io/R/combinations.R
@@ -0,0 +1,24 @@
+#' Return a data frame with each combination of the supplied inputs
+#'
+#' @param ... one or more named arguments
+#' @return a data frame
+combos <- function(...) {
+  args <- list(...)
+
+  lens <- sapply(args, length)
+
+  repeated <- lapply(1:length(args), function(i) {
+    each  <- lens[(i+1):length(args)]
+    times <- lens[0:(i-1)]
+
+    rep(
+      rep(
+        args[[i]],
+        each=prod(each)),
+      times=prod(times))
+  })
+  names(repeated) <- names(args)
+
+  repeated$stringsAsFactors <- FALSE
+  do.call(data.frame, repeated)
+}
