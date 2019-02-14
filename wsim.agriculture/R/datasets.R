b0027e65dcafaf239802197a697d2ef7bb6a8508
Dan Baston <dbaston@isciences.com>
2019-02-14 11:22:42 -0500

    Fix R CMD check issues in wsim.agriculture

---
+++ wsim.agriculture/R/datasets.R
@@ -0,0 +1,14 @@
+#' Crop names and codes used in SPAM 2010
+#' 
+#' @name spam_crops
+NULL
+
+#' Crop names and codes used in the MIRCA2000 crop calendars
+#' 
+#' @name mirca_crops
+NULL
+
+#' Example growth-stage specific loss factors
+#' 
+#' @name example_crop_factors
+NULL
--- wsim.agriculture/R/write_initial_state.R
+++ wsim.agriculture/R/write_initial_state.R
@@ -5,25 +5,30 @@
 #' @param extent     spatial extent, specified as vector of (xmin, xmax, ymin, ymax)
 #' @param crop_names values of crop dimension (e.g., maize_1, maize_1, rice_1, etc.)
 #' @param stresses   values of stress dimension (e.g., surplus, deficit, heat, cold)
+#' @param fill_zero  whether to fill \code{months_stress} and \code{cumulative_loss}
+#'                   variables with zero. If these will be fully overwritten by a later
+#'                   process, this is unnecessary.
 #' @export
-write_empty_state <- function(fname, dim, extent, subcrop_names, stresses, fill_zero=TRUE) {
-  months_stress <- array(0L, dim=c(dim, length(subcrop_names), length(stresses)))
+write_empty_state <- function(fname, dim, extent, crop_names, stresses, fill_zero=TRUE) {
+  months_stress <- array(0L, dim=c(dim, length(crop_names), length(stresses)))
   
-  cumulative_loss <- array(0L, dim=c(dim, length(subcrop_names)))
+  cumulative_loss <- array(0L, dim=c(dim, length(crop_names)))
   
-  write_vars_to_cdf(list(months_stress=months_stress),
-                    fname,
-                    extent=extent,
-                    extra_dims=list(crop=subcrop_names,
-                                    stress=stresses),
-                    prec='byte',
-                    put_data=fill_zero)
+  wsim.io::write_vars_to_cdf(
+    list(months_stress=months_stress),
+    fname,
+    extent=extent,
+    extra_dims=list(crop=crop_names,
+                    stress=stresses),
+    prec='byte',
+    put_data=fill_zero)
   
-  write_vars_to_cdf(list(cumulative_loss=cumulative_loss),
-                    fname,
-                    extent=extent,
-                    extra_dims=list(crop=subcrop_names),
-                    prec='double',
-                    append=TRUE,
-                    put_data=fill_zero)
+  wsim.io::write_vars_to_cdf(
+    list(cumulative_loss=cumulative_loss),
+    fname,
+    extent=extent,
+    extra_dims=list(crop=crop_names),
+    prec='double',
+    append=TRUE,
+    put_data=fill_zero)
 }
