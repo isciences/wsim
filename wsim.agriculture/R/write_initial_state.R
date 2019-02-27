8c75e70ca31267a87cdfd528dbee4fdb47f22fad
Dan Baston <dbaston@isciences.com>
2019-02-27 11:50:14 -0500

    Use resolution, set default args in write_initial_state

--- wsim.agriculture/R/write_initial_state.R
+++ wsim.agriculture/R/write_initial_state.R
@@ -1,7 +1,7 @@
 #' Write an empty agriculture state file to disk
 #' 
 #' @param fname      name of output file
-#' @param dim        dimensions of state (e.g., (360, 720))
+#' @param res        resolution in degrees
 #' @param extent     spatial extent, specified as vector of (xmin, xmax, ymin, ymax)
 #' @param crop_names values of crop dimension (e.g., maize_1, maize_1, rice_1, etc.)
 #' @param stresses   values of stress dimension (e.g., surplus, deficit, heat, cold)
@@ -9,11 +9,21 @@
 #'                   variables with zero. If these will be fully overwritten by a later
 #'                   process, this is unnecessary.
 #' @export
-write_empty_state <- function(fname, dim, extent, crop_names, stresses, fill_zero=TRUE) {
+write_empty_state <- function(fname, 
+                              res=0.5,
+                              extent=c(-180, 180, -90, 90),
+                              crop_names=wsim_crops$wsim_name,
+                              stresses=c('surplus', 'deficit', 'heat', 'cold'),
+                              fill_zero=TRUE) {
+  dim <- c((extent[4]-extent[3])/res, (extent[2]-extent[1])/res)
+  
   months_stress <- array(0L, dim=c(dim, length(crop_names), length(stresses)))
   
   cumulative_loss <- array(0L, dim=c(dim, length(crop_names)))
   
+  # Create file in two separate steps; unfortunately, we can't create all of
+  # the variables at once, because they have different extra dimensions, and
+  # write_vars_to_cdf doesn't support that.
   wsim.io::write_vars_to_cdf(
     list(months_stress=months_stress),
     fname,
@@ -37,7 +47,13 @@ write_empty_state <- function(fname, dim, extent, crop_names, stresses, fill_zer
 #' 
 #' @inheritParams write_empty_state
 #' @export
-write_empty_results <- function(fname, dim, extent, crop_names, fill_zero=TRUE) {
+write_empty_results <- function(fname,
+                                res=0.5,
+                                extent=c(-180, 180, -90, 90),
+                                crop_names=wsim_crops$wsim_name,
+                                fill_zero=TRUE) {
+  dim <- c((extent[4]-extent[3])/res, (extent[2]-extent[1])/res)
+  
   placeholder <- array(0L, dim=c(dim, length(crop_names)))
   
   wsim.io::write_vars_to_cdf(
