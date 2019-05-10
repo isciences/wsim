e385cee612d8a16c8eed1622ec7154f101e62c14
Cynthia <crowley@isciences.com>
2019-05-10 14:02:27 -0400

    Rewrite helpstrings for --noelectric and --noagriculture to be more precise

--- workflow/makemake.py
+++ workflow/makemake.py
@@ -52,10 +52,10 @@ def parse_args(args):
                         help='Skip model spin-up steps',
                         action='store_true')
     parser.add_argument('--noelectric',
-                        help='Do not run electricity assessment',
+                        help='Do not write steps for electric power assessment',
                         action='store_true')
     parser.add_argument('--noagriculture',
-                        help='Do not run agriculture assessment',
+                        help='Do not write steps for agriculture assessment',
                         action='store_true')
     parser.add_argument('--module',
                         help="Name of output module",
@@ -100,8 +100,8 @@ def main(raw_args):
                                     stop=args.stop,
                                     no_spinup=args.nospinup,
                                     forecasts=args.forecasts,
-                                    run_electric_power=not(args.noelectric),
-                                    run_agriculture=not(args.noagriculture))
+                                    run_electric_power=not args.noelectric,
+                                    run_agriculture=not args.noagriculture)
 
     duplicate_targets = workflow.find_duplicate_targets(steps)
     if duplicate_targets:
