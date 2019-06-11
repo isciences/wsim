36e6cae7aa581628bc76d028c5133641c0db08c3
Cynthia <crowley@isciences.com>
2019-06-11 15:47:50 -0400

    Add missing colon

--- workflow/wsim_workflow/actions.py
+++ workflow/wsim_workflow/actions.py
@@ -365,7 +365,7 @@ def fit_var(config: ConfigBase,
     else:
         param_to_read = param
 
-    if param in config.forcing_rp_vars()
+    if param in config.forcing_rp_vars():
         assert window == 1
         assert basis is None
 
