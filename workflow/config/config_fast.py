e9a60006fbc1c88d639e9e409e8a349ada565bf8
Cynthia <crowley@isciences.com>
2019-08-12 12:55:56 -0400

    Fast config abbreviate forecasts to 3 months. Add comments to config

--- workflow/config/config_fast.py
+++ workflow/config/config_fast.py
@@ -2,9 +2,15 @@
 from config_cfs import CFSConfig
 
 class CFSConfigFast(CFSConfig):
+    # Skip 6-month, >12-month integration periods:
     @staticmethod
     def integration_windows() -> List[int]:
         return [3, 12]
 
+    # Use only the first 3 forecast ensemble members:
     def forecast_ensemble_members(self, yearmon, *, lag_hours: Optional[int] = None):
-        return CFSConfig.forecast_ensemble_members(self, yearmon, lag_hours=lag_hours)[:3]
+        return CFSConfig.forecast_ensemble_members(self, yearmon, lag_hours=lag_hours)[:2]
+        
+    # Forecast out only 3 months instead of 9:
+    def forecast_targets(self, yearmon):
+        return dates.get_next_yearmons(yearmon, 3)
