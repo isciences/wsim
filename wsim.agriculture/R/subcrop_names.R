c149d1f5bc2bb7c5fd583864b1a45600ceb7bcd6
Daniel Baston <dbaston@isciences.com>
2020-06-09 20:32:57 -0400

    Add optional crop filter to subcrop_names

--- wsim.agriculture/R/subcrop_names.R
+++ wsim.agriculture/R/subcrop_names.R
@@ -17,9 +17,15 @@ subcrop_names <- function(crops, num_subcrops) {
 
 #' List the names of implemented WSIM subcrops
 #' 
+#' @param crops crops for which subcrop names should be returned, or
+#'              \code{NULL} for all crops
+#' 
 #' @export
-wsim_subcrop_names <- function() {
+wsim_subcrop_names <- function(crops=NULL) {
   x <- merge(wsim_crops, mirca_crops)
   x <- x[x$implemented, ]
+  if (!is.null(crops)) {
+    x <- x[x$wsim_name %in% crops, ]
+  }
   subcrop_names(x$wsim_name, x$mirca_subcrops)
 }
