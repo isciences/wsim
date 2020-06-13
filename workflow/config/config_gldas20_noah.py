e41228f54730c582d9f8f6354eaee9766a332d70
Daniel Baston <dbaston@isciences.com>
2020-06-12 21:48:28 -0400

    Fix basin level in GLDAS config

--- workflow/config/config_gldas20_noah.py
+++ workflow/config/config_gldas20_noah.py
@@ -32,7 +32,7 @@ class GLDAS20_NoahStatic(paths.Static):
     def global_prep_steps(self):
         return \
             ntsg_drt.global_flow_direction(filename=self.flowdir_raw, resolution=0.25) + self.extend_flowdir() + \
-            hydrobasins.basins(source_dir=self.source, filename=self.basins().file, level=5) + \
+            hydrobasins.basins(source_dir=self.source, filename=self.basins().file, level=7) + \
             hydrobasins.downstream_ids(source_dir=self.source, basins_file=self.basins().file, ids_file=self.basin_downstream().file)
 
     def extend_flowdir(self):
