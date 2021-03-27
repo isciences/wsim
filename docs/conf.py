a2640d8cc119fed76e82a9f08e55769fd73bb0e3
Daniel Baston <dbaston@gmail.com>
2021-03-26 21:08:09 -0400

    Fix Sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = [
 ]
 #math_number_all = True
 
+bibtex_bibfiles = ['references.bib']
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
